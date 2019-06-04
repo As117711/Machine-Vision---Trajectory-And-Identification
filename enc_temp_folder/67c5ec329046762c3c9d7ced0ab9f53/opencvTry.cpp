a5eabe43ee368eaf2cafdabdd1ec20120cfdee0b
Adrian <adrian.knhumanoid@gmail.com>
2019-06-04 23:56:19 +0200

    Back

--- enc_temp_folder/67c5ec329046762c3c9d7ced0ab9f53/opencvTry.cpp
+++ enc_temp_folder/67c5ec329046762c3c9d7ced0ab9f53/opencvTry.cpp
@@ -7,6 +7,10 @@
 using namespace std;
 using namespace cv;
 
+int bins = 1;
+/// Function Headers
+void median(int, void*);
+
 VideoCapture getMyMovie(string path)
 {
 	VideoCapture sourceMovie(path);
@@ -27,9 +31,14 @@ int main()
 {
 	VideoCapture sourceMovie = getMyMovie("movies/AnotherTriple.mp4");
 
+	
+
 	Mat previousFrame, frame, nextFrame, result1, result2, result3;
 	sourceMovie >> previousFrame;
 	cvtColor(previousFrame, previousFrame, COLOR_BGR2GRAY);
+
+	namedWindow("Source image", WINDOW_AUTOSIZE);
+	createTrackbar("* Median filter: ", "Source image", 0, 100);
 
 	while(1)
 	{
@@ -47,21 +56,12 @@ int main()
 		//dilate(result1, result1, Mat());
 
 		imshow("result1",result1);
-		imshow("frame", frame);
+		imshow("frame", frame); 
+
+		
 		waitKey(0);
 		previousFrame = frame.clone();
 	}
 
 	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
