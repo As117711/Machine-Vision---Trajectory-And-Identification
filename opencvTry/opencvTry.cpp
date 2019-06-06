a1aaf51cf5dc438e5cb936370b07c38325b0816a
Adrian <adrian.knhumanoid@gmail.com>
2019-06-06 02:36:09 +0200

    Adjusting Parameters.

--- opencvTry/opencvTry.cpp
+++ opencvTry/opencvTry.cpp
@@ -43,33 +43,34 @@ int main()
 	cvtColor(previousFrame, previousFrame, COLOR_BGR2GRAY);
 	
 	
-	while(1)
+	while (1)
 	{
 		if (!sourceMovie.read(frame)) { break; }
 
 		sourceMovie >> frame;
-		
+
 		cvtColor(frame, frame, COLOR_BGR2GRAY);
 		absdiff(frame, previousFrame, result1);
 		getSomeFunWithTreshold(result1, 20);
-		
-		erode(result1, result1, getStructuringElement(MORPH_RECT,Size(4,4)));
-		dilate(result1, result1, getStructuringElement(MORPH_RECT,Size(2,2)));
-		
+
+		erode(result1, result1, getStructuringElement(MORPH_RECT, Size(4, 4)));
+		dilate(result1, result1, getStructuringElement(MORPH_RECT, Size(2, 2)));
+
 
 		findContours(result1, vectors, RETR_LIST, CHAIN_APPROX_SIMPLE);
-		
+
+		ClassificationTree* Tree = new ClassificationTree();
 
 		for (int i = 0; i < vectors.size(); i++)
 		{
 			if (contourArea(vectors[i]) > 200) {
 				//drawContours(finalFrame, vectors, i, Scalar(255, 0, 0), 2); Contours 
 				valuableVectors.push_back(i);
-				
+
 				approxPolyDP(Mat(vectors[i]), contours_poly, 3, true);
 				boundRect = boundingRect(Mat(contours_poly));
-				
-				cout << "S: " << contours_poly.size()<<" :S";
+
+				cout << "S: " << contours_poly.size() << " :S";
 
 				Moments mu = moments(vectors[i], false);
 				Point pos = Point(mu.m10 / mu.m00, mu.m01 / mu.m00);
@@ -79,10 +80,26 @@ int main()
 
 				circle(finalFrame, Point(boundRect.x + boundRect.width / 2, boundRect.y + boundRect.height / 2), 3, Scalar(250, 125, 125), 3);
 				circle(finalFrame, pos, 3, Scalar(250, 255, 125), 5);
+
+				Tree->AddPoint(pos);
+
 				imshow("finalFrame", finalFrame);
 				waitKey(0);
-					
+
 			}
+		}
+
+		int counter = Tree->getGroupsSize();
+		for (int i = 0; i < counter; i++)
+		{
+			vector <Point> actual = Tree->getPointsFromGroupById(i),test;
+			approxPolyDP(Mat(actual), test, 3, true);
+			boundRect = boundingRect(Mat(test));
+			Moments mu = moments(vectors[i], false);
+			Point pos = Point(mu.m10 / mu.m00, mu.m01 / mu.m00);
+			circle(finalFrame, pos, 3, Scalar(255, 255, 255), 5);
+			imshow("finalFrame", finalFrame);
+			
 		}
 
 		imshow("result1",result1);
